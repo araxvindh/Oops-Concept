146514b57066e784d29097cfc3fde79bbcc2ee81
araxvindh <aravindhswamy22@gmail.com>
2025-01-01 23:49:36 +0530

    Encapsulation add

---
+++ Oops(final)/Encapsulation.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class Student
+{
+    private:
+        string name;
+        int age;
+        int height;
+
+    public:
+        int getAge()
+        {
+            return this->age;
+        }
+
+
+
+};
+
+int main()
+{
+    Student first;
+
+    return 0;
+}
+
