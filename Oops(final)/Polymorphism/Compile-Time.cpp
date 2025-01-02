64d5e8a1e0e5359d11999f0c7171a50c9ac1e752
araxvindh <aravindhswamy22@gmail.com>
2025-01-02 19:55:18 +0530

    update

--- Oops(final)/Polymorphism/Compile-Time.cpp
+++ Oops(final)/Polymorphism/Compile-Time.cpp
@@ -27,15 +27,53 @@ class A
 
 
 
+class B
+{
+
+    public:
+        int a;
+        int b;
+    public :
+        int add()
+        {
+            return a+b;
+        }
+
+        void operator+(B &obj)
+        {
+            int val1=this->a;
+            int val2=obj.a;
+
+            cout<<"output"<< val2-val1<<endl;
+        }
+
+        void operator() ()
+        {
+            cout<<"bracket is called"<< this->a<<endl;
+        }
+};
+
+
+
+
 int main()
 {
 
+
+
     A ob1;
 
-    ob1.sayHello();
-     ob1.sayHello("Aravindh");
-          ob1.sayHello("Aravindh",1);
+    //ob1.sayHello();
+    //ob1.sayHello("Aravindh");
+    //ob1.sayHello("Aravindh",1);
 
+    B ob2,ob3;
 
+    ob2.a=4;
+    ob3.a=7;
+
+    ob2+ob3;
+    ob2();
+    ob3();
     return 0;
 }
